22083a95eaa7c7184279b4cbf778eed6c0cb0e43
nesscollege2016 <nesscollege2016@gmail.com>
2016-08-01 09:02:39 +0300

    inserts

--- app/src/main/java/ness/tomerbu/edu/sqlitedemosparttwo/ScrollingActivity.java
+++ app/src/main/java/ness/tomerbu/edu/sqlitedemosparttwo/ScrollingActivity.java
@@ -1,16 +1,22 @@
 package ness.tomerbu.edu.sqlitedemosparttwo;
 
+import android.content.ContentValues;
 import android.os.Bundle;
-import android.provider.ContactsContract;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
-import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.EditText;
 
 public class ScrollingActivity extends AppCompatActivity {
+
+    EditText etSongName;
+    EditText etArtist;
+    EditText etImage;
+    EditText etDuration;
+    EditText etID;
+    EditText etAlbum;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,15 +25,12 @@ public class ScrollingActivity extends AppCompatActivity {
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
+        etSongName = (EditText) findViewById(R.id.etTitle);
+        etArtist = (EditText) findViewById(R.id.etArtist);
+        etImage = (EditText) findViewById(R.id.etImage);
+        etDuration = (EditText) findViewById(R.id.etDuration);
+        etID = (EditText) findViewById(R.id.etID);
+        etAlbum = (EditText) findViewById(R.id.etAlbum);
     }
 
     @Override
@@ -52,6 +55,23 @@ public class ScrollingActivity extends AppCompatActivity {
     }
 
     public void insert(View view) {
+        SongDBHelper helper = new SongDBHelper(this);
+        ContentValues values = new ContentValues();
+        values.put(SongContract.Song.COL_ALBUM, getAlbum());
+        values.put(SongContract.Song.COL_ARTIST, getArtist());
+        values.put(SongContract.Song.COL_DURATION, getDuration());
+        values.put(SongContract.Song.COL_IMAGE, getImage());
+        values.put(SongContract.Song.COL_TITLE, getSongTitle());
+        //values.put(SongContract.Song.COL_ID, getID());
+
+
+
+        helper.getWritableDatabase().insert(
+                SongContract.Song.TABLE_NAME,
+                null,
+                values
+        );
+
     }
 
     public void delete(View view) {
@@ -61,5 +81,30 @@ public class ScrollingActivity extends AppCompatActivity {
     }
 
     public void read(View view) {
+    }
+
+    public String getAlbum() {
+        return etAlbum.getText().toString();
+    }
+
+    public String getArtist() {
+        return etArtist.getText().toString();
+    }
+
+    public String getDuration() {
+        return etDuration.getText().toString();
+    }
+
+    public String getImage() {
+        return etImage.getText().toString();
+    }
+
+
+    public String getSongTitle() {
+        return etSongName.getText().toString();
+    }
+
+    public String getID() {
+        return etID.getText().toString();
     }
 }
